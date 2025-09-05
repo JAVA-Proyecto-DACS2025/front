e59cbb6c40c203c148cccf38b3b7527d2d8b2026
lucaschneider@gmail.com <lucaschneider@gmail.com>
2025-09-05 18:41:07 -0300

    segundo commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,10 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-
+import { provideHttpClient } from '@angular/common/http'; // <-- Importa esto
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(), // <-- ¡Añádelo aquí para que esté disponible globalmente!
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
